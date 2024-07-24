59f86db33cbef1039e64aa146f11bcc5977ada56
satisfy <laube_marco@yahoo.de>
2024-07-24 18:20:18 +0200

    feat(fix): issue #119
    https://github.com/satisfyu/Let-s-Do-Hub/issues/119

--- common/src/main/java/net/satisfy/beachparty/block/BeachTowelBlock.java
+++ common/src/main/java/net/satisfy/beachparty/block/BeachTowelBlock.java
@@ -1,6 +1,7 @@
 package net.satisfy.beachparty.block;
 
-import de.cristelknight.doapi.common.util.ChairUtil;
+import de.cristelknight.doapi.common.entity.ChairEntity;
+import de.cristelknight.doapi.common.registry.DoApiEntityTypes;
 import de.cristelknight.doapi.common.util.GeneralUtil;
 import net.minecraft.ChatFormatting;
 import net.minecraft.Util;
@@ -9,38 +10,31 @@ import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
-import net.minecraft.world.level.block.LevelEvent;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BedPart;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
-@SuppressWarnings("deprecation")
-public class BeachTowelBlock extends HorizontalDirectionalBlock {
+public class BeachTowelBlock extends BeachChairBlock {
 	public static final EnumProperty<BedPart> PART = BlockStateProperties.BED_PART;
 	private static final Supplier<VoxelShape> voxelShapeSupplier = () -> {
 		VoxelShape shape = Shapes.empty();
@@ -61,69 +55,30 @@ public class BeachTowelBlock extends HorizontalDirectionalBlock {
 		return SHAPE.get(direction);
 	}
 
-	public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
-
 	public BeachTowelBlock(Properties settings) {
 		super(settings);
 		this.registerDefaultState(this.defaultBlockState().setValue(FACING, Direction.NORTH).setValue(PART, BedPart.FOOT));
 	}
 
-	public @NotNull BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor world, BlockPos pos, BlockPos neighborPos) {
-		if (direction == getDirectionTowardsOtherPart(state.getValue(PART), state.getValue(FACING))) {
-			return neighborState.is(this) && neighborState.getValue(PART) != state.getValue(PART) ? state : Blocks.AIR.defaultBlockState();
-		} else {
-			return super.updateShape(state, direction, neighborState, world, pos, neighborPos);
-		}
-	}
-
-	private static Direction getDirectionTowardsOtherPart(BedPart part, Direction direction) {
-		return part == BedPart.FOOT ? direction : direction.getOpposite();
-	}
-
-	public void playerWillDestroy(Level world, BlockPos pos, BlockState state, Player player) {
-		if (!world.isClientSide && player.isCreative()) {
-			removeOtherPart(world, pos, state, player);
-		}
-
-		super.playerWillDestroy(world, pos, state, player);
-	}
-
-	@Nullable
-	public BlockState getStateForPlacement(BlockPlaceContext ctx) {
-		Direction direction = ctx.getHorizontalDirection();
-		BlockPos blockPos = ctx.getClickedPos();
-		BlockPos blockPos2 = blockPos.relative(direction);
-		Level world = ctx.getLevel();
-		return world.getBlockState(blockPos2).canBeReplaced(ctx) && world.getWorldBorder().isWithinBounds(blockPos2) ? this.defaultBlockState().setValue(FACING, direction) : null;
-	}
-
-	public static Direction getOppositePartDirection(BlockState state) {
-		Direction direction = state.getValue(FACING);
-		return state.getValue(PART) == BedPart.HEAD ? direction.getOpposite() : direction;
-	}
-
-	public void setPlacedBy(Level world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
-		super.setPlacedBy(world, pos, state, placer, itemStack);
-		if (!world.isClientSide) {
-			placeOtherPart(world, pos, state);
-		}
-	}
-
 	@Override
-	public @NotNull InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand
-			hand, BlockHitResult hit) {
-		if (!world.isClientSide) {
-			if (state.getValue(PART) == BedPart.FOOT) {
-				return ChairUtil.onUse(world, player, hand, hit, -0.4);
-			} else {
-				BlockPos mainPartPos = pos.relative(state.getValue(FACING).getOpposite());
-				BlockState mainPartState = world.getBlockState(mainPartPos);
-				if (mainPartState.is(this) && mainPartState.getValue(PART) == BedPart.FOOT) {
-					return mainPartState.use(world, player, hand, hit);
-				}
+	public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+		if (world.isClientSide) {
+			return InteractionResult.SUCCESS;
+		} else {
+			BlockPos blockPos = hit.getBlockPos();
+			if (!isOccupied(world, blockPos) && player.getItemInHand(hand).isEmpty()) {
+				CustomChairEntity chairEntity = new CustomChairEntity(DoApiEntityTypes.CHAIR.get(), world, blockPos, player);
+				chairEntity.moveTo((double)blockPos.getX() + 0.5, (double)blockPos.getY() + 0.25 - 0.4, (double)blockPos.getZ() + 0.5, 0.0F, 0.0F);
+				world.addFreshEntity(chairEntity);
+				player.startRiding(chairEntity);
+				return InteractionResult.SUCCESS;
 			}
+			return InteractionResult.PASS;
 		}
-		return InteractionResult.SUCCESS;
+	}
+
+	private boolean isOccupied(Level world, BlockPos pos) {
+		return !world.getEntitiesOfClass(ChairEntity.class, new AABB(pos)).isEmpty();
 	}
 
 	@Override
@@ -131,28 +86,27 @@ public class BeachTowelBlock extends HorizontalDirectionalBlock {
 		builder.add(FACING, PART);
 	}
 
-	private void placeOtherPart(Level world, BlockPos pos, BlockState state) {
-		BlockPos blockPos = pos.relative(state.getValue(FACING));
-		world.setBlock(blockPos, state.setValue(PART, BedPart.HEAD), Block.UPDATE_ALL);
-		world.blockUpdated(pos, Blocks.AIR);
-		state.updateNeighbourShapes(world, pos, Block.UPDATE_ALL);
-	}
-
-	private void removeOtherPart(Level world, BlockPos pos, BlockState state, Player player) {
-		BedPart bedPart = state.getValue(PART);
-		if (bedPart == BedPart.FOOT) {
-			BlockPos blockPos = pos.relative(getDirectionTowardsOtherPart(bedPart, state.getValue(FACING)));
-			BlockState blockState = world.getBlockState(blockPos);
-			if (blockState.is(this) && blockState.getValue(PART) == BedPart.HEAD) {
-				world.setBlock(blockPos, Blocks.AIR.defaultBlockState(), Block.UPDATE_ALL | Block.UPDATE_SUPPRESS_DROPS);
-				if (player != null)
-					world.levelEvent(player, LevelEvent.PARTICLES_DESTROY_BLOCK, blockPos, Block.getId(blockState));
-			}
-		}
-	}
-
 	@Override
 	public void appendHoverText(ItemStack itemStack, BlockGetter world, List<Component> tooltip, TooltipFlag tooltipContext) {
 		tooltip.add(Component.translatable("tooltip.beachparty.canbeplaced").withStyle(ChatFormatting.ITALIC, ChatFormatting.GRAY));
+	}
+
+	public static class CustomChairEntity extends ChairEntity {
+		private final BlockPos blockPos;
+		private final Player player;
+
+		public CustomChairEntity(EntityType<? extends ChairEntity> entityType, Level world, BlockPos blockPos, Player player) {
+			super(entityType, world);
+			this.blockPos = blockPos;
+			this.player = player;
+		}
+
+		@Override
+		public void ejectPassengers() {
+			super.ejectPassengers();
+			if (!this.level().isClientSide) {
+				this.player.teleportTo(blockPos.getX() + 0.5, blockPos.getY() + 2, blockPos.getZ() + 0.5);
+			}
+		}
 	}
 }
