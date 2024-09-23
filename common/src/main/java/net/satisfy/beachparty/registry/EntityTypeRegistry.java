ae1dd125dfcd7a145eed9e322b2940b59f7286f4
satisfy <laube_marco@yahoo.de>
2024-09-23 12:09:45 +0200

    merging

--- common/src/main/java/net/satisfy/beachparty/registry/EntityTypeRegistry.java
+++ common/src/main/java/net/satisfy/beachparty/registry/EntityTypeRegistry.java
@@ -7,12 +7,10 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.entity.HangingSignBlockEntity;
-import net.minecraft.world.level.block.entity.SignBlockEntity;
 import net.satisfy.beachparty.Beachparty;
 import net.satisfy.beachparty.block.entity.*;
-import net.satisfy.beachparty.entity.BeachpartyBoat;
-import net.satisfy.beachparty.entity.BeachpartyChestBoat;
+import net.satisfy.beachparty.entity.BeachpartyBoatEntity;
+import net.satisfy.beachparty.entity.BeachpartyChestBoatEntity;
 import net.satisfy.beachparty.entity.ChairEntity;
 import net.satisfy.beachparty.entity.CoconutEntity;
 import net.satisfy.beachparty.platform.PlatformHelper;
@@ -30,14 +28,10 @@ public final class EntityTypeRegistry {
     public static final RegistrySupplier<BlockEntityType<MiniFridgeBlockEntity>> MINI_FRIDGE_BLOCK_ENTITY = registerBlockEntity("mini_fridge", () -> BlockEntityType.Builder.of(MiniFridgeBlockEntity::new, ObjectRegistry.MINI_FRIDGE.get()).build(null));
     public static final RegistrySupplier<BlockEntityType<CabinetBlockEntity>> CABINET_BLOCK_ENTITY = registerBlockEntity("cabinet", () -> BlockEntityType.Builder.of(CabinetBlockEntity::new, CABINET.get()).build(null));
 
-    public static final Supplier<BlockEntityType<SignBlockEntity>> SIGN = PlatformHelper.registerBlockEntityType("sign", () -> PlatformHelper.createBlockEntityType(BeachpartySignBlockEntity::new, PALM_SIGN.get(), PALM_WALL_SIGN.get()));
-
-    public static final Supplier<BlockEntityType<HangingSignBlockEntity>> HANGING_SIGN = PlatformHelper.registerBlockEntityType("hanging_sign", () -> PlatformHelper.createBlockEntityType(BeachpartyHangingSignBlockEntity::new, PALM_HANGING_SIGN.get(), PALM_WALL_HANGING_SIGN.get()));
-
     public static final RegistrySupplier<EntityType<ChairEntity>> CHAIR = registerEntity("chair", () -> EntityType.Builder.of(ChairEntity::new, MobCategory.MISC).sized(0.001F, 0.001F).build(new BeachpartyIdentifier("chair").toString()));
     public static final RegistrySupplier<EntityType<CoconutEntity>> COCONUT = registerEntity("coconut", () -> EntityType.Builder.<CoconutEntity>of(CoconutEntity::new, MobCategory.MISC).sized(0.25f, 0.25f).build(new BeachpartyIdentifier("coconut").toString()));
-    public static final Supplier<EntityType<BeachpartyBoat>> BOAT = PlatformHelper.registerBoatType("boat", BeachpartyBoat::new, MobCategory.MISC, 1.375F, 0.5625F, 10);
-    public static final Supplier<EntityType<BeachpartyChestBoat>> CHEST_BOAT = PlatformHelper.registerBoatType("chest_boat", BeachpartyChestBoat::new, MobCategory.MISC, 1.375F, 0.5625F, 10);
+    public static final Supplier<EntityType<BeachpartyBoatEntity>> BOAT = PlatformHelper.registerBoatType("boat", BeachpartyBoatEntity::new, MobCategory.MISC, 1.375F, 0.5625F, 10);
+    public static final Supplier<EntityType<BeachpartyChestBoatEntity>> CHEST_BOAT = PlatformHelper.registerBoatType("chest_boat", BeachpartyChestBoatEntity::new, MobCategory.MISC, 1.375F, 0.5625F, 10);
 
 
     private static <T extends BlockEntityType<?>> RegistrySupplier<T> registerBlockEntity(final String path, final Supplier<T> type) {
